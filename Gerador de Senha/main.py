520f69c26c3db09f2208d5fb3107f9dc1d5f9df5
André <andre.luis.borato@gmail.com>
2026-07-09 11:45:26 -0300

    3

--- Gerador de Senha/main.py
+++ Gerador de Senha/main.py
@@ -1,4 +1,4 @@
-import random
+import string
 import secrets
 
 
@@ -8,7 +8,11 @@ print("1 - Só números")
 print("2 - Só letras")
 print("3 - Misturado")
 
-senha1 = secrets.choice
+tamanho_senha = 8
+
+caracteres1 = string.ascii_letters
+
+senha1 = "".join(secrets.choice(caracteres1)) for _ in range(tamanho_senha)
 
 tipo = int(input("Escolha: "))
 
